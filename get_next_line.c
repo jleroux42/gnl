a2eb29ac2062686aa8bed4bc6666cdc75c48b3d5
Joachim Leroux <lerouxjoachim@gmail.com>
2021-10-29 13:22:21 +0200

    Add file to search in and print jsut a bit

--- get_next_line.c
+++ get_next_line.c
@@ -1,23 +1,20 @@
-#define BUFFER_SIZE 64
+#define BUFFER_SIZE 4
 
 #include <libc.h>
 
 char	*get_next_line(int fd)
 {
 	static char			*buffer;
-	size_t				retour_read;
+	int					retour_read;
 	int					i;
 	char				*line;
-	char				**start;
 
-	start = &buffer;
-	buffer = malloc(1000);
+	buffer = malloc(BUFFER_SIZE * sizeof(char) + 1);
 	line = malloc(1000);
-	read(fd, buffer, BUFFER_SIZE);
+	retour_read = read(fd, buffer, BUFFER_SIZE);
 	i = 0;
 	while (buffer[i] != '\0' && buffer[i] != '\n' && i <= BUFFER_SIZE)
 		i++;
-	buffer -= BUFFER_SIZE -1;
 	line = strncpy(line, buffer, i);
 	return (line);
 }
@@ -25,5 +22,5 @@ char	*get_next_line(int fd)
 int		main(int argc, char **argv)
 {
 	int		fd = open(argv[1], O_RDONLY);
-	printf("%s", get_next_line(fd));
+	printf("%s\n", get_next_line(fd));
 }
