d43c580e3e0480d2f0236625faaed104d7c98b53
Joachim Leroux <lerouxjoachim@gmail.com>
2021-11-02 13:57:11 +0100

    Affiche tout

--- get_next_line.c
+++ get_next_line.c
@@ -1,26 +1,47 @@
-#define BUFFER_SIZE 4
+#define BUFFER_SIZE 42
 
 #include <libc.h>
 
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	s_len;
+
+        if (!s)
+          return (NULL);
+	s_len = strlen(s);
+	if (start > s_len)
+		len = 0;
+	else if (start + len > s_len)
+		len = s_len - start;
+	sub = malloc(1 + len * sizeof(*s));
+	if (!sub)
+		return (NULL);
+	memmove(sub, s + start, len);
+	sub[len] = 0;
+	return (sub);
+}
+
 char	*get_next_line(int fd)
 {
-	static char			*buffer;
-	int					retour_read;
-	int					i;
-	char				*line;
+	static char		*buffer;
+	static int end;
+	static int start;
+	char *line;
 
-	buffer = malloc(BUFFER_SIZE * sizeof(char) + 1);
-	line = malloc(1000);
-	retour_read = read(fd, buffer, BUFFER_SIZE);
-	i = 0;
-	while (buffer[i] != '\0' && buffer[i] != '\n' && i <= BUFFER_SIZE)
-		i++;
-	line = strncpy(line, buffer, i);
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	read(fd, buffer, BUFFER_SIZE);
+	start = end;
+	while (buffer[end] != '\0')
+		end++;
+	line = ft_substr(buffer, start, end - start);
+	free(buffer);
 	return (line);
 }
 
 int		main(int argc, char **argv)
 {
 	int		fd = open(argv[1], O_RDONLY);
-	printf("%s\n", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	close(fd);
 }
